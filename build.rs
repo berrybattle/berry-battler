33224b9f23777e00d4f84c60e5bebd08a556816a
Aven Arlington <aven.arlington@gmail.com>
2024-01-23 01:07:41 -0800

    Customizing message to contain data

--- build.rs
+++ build.rs
@@ -3,7 +3,11 @@ use std::{env, path::PathBuf};
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_build::configure()
-        .file_descriptor_set_path(out_dir.join("game_update_descriptor.bin"))
-        .compile(&["proto/game_update.proto"], &["proto"])
+        .build_server(true)
+        .file_descriptor_set_path(out_dir.join("game_server_descriptor.bin"))
+        .compile(
+            &["proto/game_data.proto", "proto/game_update.proto"],
+            &["proto"],
+        )
         .unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,47 @@
-use hello::say_client::SayClient;
-use hello::SayRequest;
+use game_server::update_game_state_server::{UpdateGameState, UpdateGameStateServer};
+use game_server::{UpdateStateRequest, UpdateStatus, UpdatedStateResponse};
+use prost_types::Timestamp;
+use tonic::{transport::Server, Request, Response, Status};
 
-pub mod hello {
-    tonic::include_proto!("hello");
+pub mod game_server {
+    tonic::include_proto!("game_server");
+}
+
+#[derive(Default)]
+pub struct UpdateGameStateService {}
+
+#[tonic::async_trait]
+impl UpdateGameState for UpdateGameStateService {
+    async fn update(
+        &self,
+        request: Request<UpdateStateRequest>,
+    ) -> Result<Response<UpdatedStateResponse>, Status> {
+        println!("Got a request from {:?}", request.remote_addr());
+
+        let mut processed_data = request.get_ref().data.clone();
+        processed_data.push_str("_processed");
+        let reply = UpdatedStateResponse {
+            //message: format!("Hello {}!", request.get_ref().name),
+            updated_status: UpdateStatus::Finished as i32,
+            update_id: request.get_ref().update_id.clone(),
+            timestamp: Some(Timestamp::default()),
+            updated_data: processed_data,
+        };
+        Ok(Response::new(reply))
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = SayClient::connect("http://[::1]:50051").await?;
+    let addr = "[::1]:50051".parse().unwrap();
 
-    let request = tonic::Request::new(SayRequest {
-        name: "Tonic".into(),
-    });
+    println!("Server listening on {}", addr);
 
-    let response = client.send(request).await?;
-
-    println!("RESPONSE={:?}", response);
+    let say = UpdateGameStateService::default();
+    Server::builder()
+        .add_service(UpdateGameStateServer::new(say))
+        .serve(addr)
+        .await?;
 
     Ok(())
 }
